bd8fc17c900262d3556f242737ad5ddc4c35df6d
LineGuld <73698741+LineGuld@users.noreply.github.com>
2022-05-20 19:53:08 +0200

    Endpoint legeplads

--- TerraEyes-BusinessServer/DBNetworking/DbConnection.cs
+++ TerraEyes-BusinessServer/DBNetworking/DbConnection.cs
@@ -9,12 +9,31 @@ namespace TerraEyes_BusinessServer.DBNetworking
 {
     public class DbConnection : IDbConnect
     {
-        private string uri = "http://terraeyesdbserver.eu-central-1.elasticbeanstalk.com/";
+        private string uri = "https://terraeyes-db.azurewebsites.net/";
         public async Task<List<TemperatureMeasurement>> GetTemperatureFromDb(string userId)
         {
             using HttpClient client = new HttpClient();
-            HttpResponseMessage responseMessage = await client.GetAsync($"{uri}temperatures");
-            
+            HttpResponseMessage responseMessage = await client.GetAsync($"{uri}temperatures/{userId}");
+
+            if (!responseMessage.IsSuccessStatusCode)
+            {
+                throw new Exception($"StatusCode: {responseMessage.StatusCode}");
+            }
+
+            string listAsString = await responseMessage.Content.ReadAsStringAsync();
+            List<TemperatureMeasurement> temperatures = JsonSerializer.Deserialize<List<TemperatureMeasurement>>(listAsString, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+
+            return temperatures;
+        }
+
+        public async Task<List<TemperatureMeasurement>> GetTerrariumTemperaturesFromDb(string userId, string terrariumId)
+        {
+            using HttpClient client = new HttpClient();
+            HttpResponseMessage responseMessage = await client.GetAsync($"{uri}temperatures/{userId}/{terrariumId}");
+
             if (!responseMessage.IsSuccessStatusCode)
             {
                 throw new Exception($"StatusCode: {responseMessage.StatusCode}");
@@ -32,8 +51,8 @@ namespace TerraEyes_BusinessServer.DBNetworking
         public async Task<List<HumidityMeasurement>> GetHumidityFromDb(string userId)
         {
             using HttpClient client = new HttpClient();
-            HttpResponseMessage responseMessage = await client.GetAsync($"{uri}temperatures");
-            
+            HttpResponseMessage responseMessage = await client.GetAsync($"{uri}humidity/{userId}");
+
             if (!responseMessage.IsSuccessStatusCode)
             {
                 throw new Exception($"StatusCode: {responseMessage.StatusCode}");
